c2ffd0539f8bcbef716550042dcfb6cc76b59d62
miloskv02 <shomikv22@gmail.com>
2024-06-10 13:49:05 +0200

    l18n (srpski, engleski)

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,31 @@ namespace GUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SwitchLanguage(string cultureCode)
+        {
+            var culture = new CultureInfo(cultureCode);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            var dict = new ResourceDictionary
+            {
+                Source = cultureCode switch
+                {
+                    "sr" => new Uri("Resources/StringResources.srb.xaml", UriKind.Relative),
+                    _ => new Uri("Resources/StringResources.en.xaml", UriKind.Relative)
+                }
+            };
+
+            this.Resources.MergedDictionaries[0] = dict;
+        }
+        
+        private void LanguageSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count <= 0) return;
+            var selectedLanguage = (e.AddedItems[0] as ComboBoxItem)?.Tag.ToString();
+            SwitchLanguage(selectedLanguage);
         }
     }
 }
